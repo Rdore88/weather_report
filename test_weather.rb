219f6bbd0a3398e83eb29d32b6f694c5b04c0c92
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-03-07 10:16:15 -0500

    updated my tests to make them work.

--- test_weather.rb
+++ test_weather.rb
@@ -15,26 +15,26 @@ class WeatherTest < Minitest::Test
   def test_get_weather
     stub_request(:get, 'http://api.wunderground.com/api/a0fad745509bd909/conditions/settings/q/20011.json')
   .to_return(body: File.read("./stubbed_requests/conditions_20011.json"), headers: { 'Content-Type' => "application/json" })
-  assert_equal "38.947712", Weather.new.get_weather["current_observation"]["observation_location"]["latitude"]
+  assert_equal "38.947712", Weather.new.get_weather(20011)["current_observation"]["observation_location"]["latitude"]
   end
 
 
   def test_sunrise_sunset
     stub_request(:get, 'http://api.wunderground.com/api/a0fad745509bd909/astronomy/settings/q/20011.json')
   .to_return(body: File.read("./stubbed_requests/astronomy_20011.json"), headers: { 'Content-Type' => "application/json" })
-  assert_equal  "18", Weather.new.sunrise_sunset["sun_phase"]["sunset"]["hour"]
+  assert_equal  "18", Weather.new.sunrise_sunset(20011)["sun_phase"]["sunset"]["hour"]
   end
 
   def test_10_day_forecast
     stub_request(:get, 'http://api.wunderground.com/api/a0fad745509bd909/forecast10day/settings/q/20011.json')
   .to_return(body: File.read("./stubbed_requests/forecast_20011.json"), headers: { 'Content-Type' => "application/json" })
-  assert_equal "Monday", Weather.new.ten_day_forecast["forecast"]["txt_forecast"]["forecastday"][0]["title"]
+  assert_equal "Monday", Weather.new.ten_day_forecast(20011)["forecast"]["txt_forecast"]["forecastday"][0]["title"]
   end
 
   def test_current_alerts
     stub_request(:get, 'http://api.wunderground.com/api/a0fad745509bd909/alerts/settings/q/20011.json')
   .to_return(body: File.read("./stubbed_requests/current_alerts_20011.json"), headers: { 'Content-Type' => "application/json" })
-  assert_equal [], Weather.new.current_alerts["alerts"]
+  assert_equal [], Weather.new.current_alerts(20011)["alerts"]
   end
 
 end
